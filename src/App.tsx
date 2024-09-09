8d7e5c68548b21cd94e5f4f57f31d12993bfbb61
Gabriel Perez <gperez@iastate.edu>
2024-09-09 16:09:18 -0600

    First iteration of background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import Header from './components/tsx/Header';
+import Background from './components/tsx/Background';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Background />
       <Header />
 
     </div>
