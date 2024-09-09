a20515dd115eb31696e52c30d3b3a413e88aea53
Gabriel Perez <gperez@iastate.edu>
2024-09-09 15:29:38 -0600

    First iteration of the header

--- src/components/tsx/Header.tsx
+++ src/components/tsx/Header.tsx
@@ -3,8 +3,17 @@ import '../styling/Header.css'; // Styling
 
 const Header: React.FC = () => {
   return (
-    <header className="header">
-      <h1>My Portfolio</h1>
+    <header className="header-title">
+      <h1>Gabriel Perez</h1>
+      <h2>Developer</h2>
+      <nav className="header-nav">
+        <ul>
+             <li><a href="#home" className="nav-link">Home</a></li>
+             <li><a href="#about" className="nav-link">About</a></li>
+             <li><a href="#projects" className="nav-link">Projects</a></li>
+             <li><a href="#contact" className="nav-link">Contact</a></li>
+        </ul>
+       </nav>
     </header>
   );
 };
