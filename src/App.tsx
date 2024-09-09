7c2d437cea95f348d13ab17966c9cf0f0e5ed5c2
Gabriel Perez <gperez@iastate.edu>
2024-09-09 14:53:14 -0600

    Header test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Portfolio</h1>
-        <p>Feel free to explore my work and projects!</p>
-      </header>
+      <Header />
+      
     </div>
   );
 }
---
+++ src/components/tsx/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../tsx/Header.css'; // Styling
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1>My Portfolio</h1>
+    </header>
+  );
+};
+
+export default Header;
