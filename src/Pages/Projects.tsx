85285f71cfdef543a3b3d224034fd4bb8c1db8a3
Gabriel Perez <gperez@iastate.edu>
2024-09-23 23:35:48 -0500

    First iteration of projects

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import './Projects.css'; // Import custom styling
+
 const Projects: React.FC = () => {
-    return (
-    <div></div>
-    )
-}
+  return (
+    <div className="projects-section">
+        <h2>Projects</h2>
+        <p>
+          🚧 Work in progress! 🚧
+        </p>
+        <p>
+          My projects are currently being showcased on my{' '}
+          <a href="https://github.com/GabrielPerezCSDev" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>.
+          Please check there for updates and project details!
+        </p>
+    </div>
+  );
+};
 
 export default Projects;
